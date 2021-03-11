9423acf526fad5cebcf867d532cde42f765d8327
Jason C.H <ctrysbita@outlook.com>
2021-03-11 15:25:05 +0800

    Add instance

---
+++ lib/src/instance.dart
@@ -0,0 +1,91 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'type.dart';
+
+part 'instance.g.dart';
+
+enum ReleaseStatus {
+  debug,
+  preview,
+  released,
+}
+
+@JsonSerializable()
+class InstanceEntry {
+  final ReleaseStatus status;
+  final String model;
+  final String type;
+  final int version;
+
+  InstanceEntry({
+    required this.status,
+    required this.model,
+    required this.type,
+    required this.version,
+  });
+
+  factory InstanceEntry.fromJson(Map<String, dynamic> json) =>
+      _$InstanceEntryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InstanceEntryToJson(this);
+}
+
+@JsonSerializable()
+class InstanceSpec {
+  final SpecType type;
+  final String description;
+  final List<InstanceServiceSpec> services;
+
+  InstanceSpec({
+    required this.type,
+    required this.description,
+    required this.services,
+  });
+
+  factory InstanceSpec.fromJson(Map<String, dynamic> json) =>
+      _$InstanceSpecFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InstanceSpecToJson(this);
+}
+
+@JsonSerializable()
+class InstanceServiceSpec {
+  final int iid;
+  final SpecType type;
+  final String description;
+
+  @JsonKey(defaultValue: <InstancePropertySpec>[])
+  final List<InstancePropertySpec> properties;
+
+  InstanceServiceSpec({
+    required this.iid,
+    required this.type,
+    required this.description,
+    this.properties = const [],
+  });
+
+  factory InstanceServiceSpec.fromJson(Map<String, dynamic> json) =>
+      _$InstanceServiceSpecFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InstanceServiceSpecToJson(this);
+}
+
+@JsonSerializable()
+class InstancePropertySpec {
+  final int iid;
+  final SpecType type;
+  final String description;
+  final String format;
+
+  InstancePropertySpec({
+    required this.iid,
+    required this.type,
+    required this.description,
+    required this.format,
+  });
+
+  factory InstancePropertySpec.fromJson(Map<String, dynamic> json) =>
+      _$InstancePropertySpecFromJson(json);
+
+  Map<String, dynamic> toJson() => _$InstancePropertySpecToJson(this);
+}
---
+++ lib/src/instance.g.dart
@@ -0,0 +1,113 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'instance.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+InstanceEntry _$InstanceEntryFromJson(Map<String, dynamic> json) {
+  return InstanceEntry(
+    status: _$enumDecode(_$ReleaseStatusEnumMap, json['status']),
+    model: json['model'] as String,
+    type: json['type'] as String,
+    version: json['version'] as int,
+  );
+}
+
+Map<String, dynamic> _$InstanceEntryToJson(InstanceEntry instance) =>
+    <String, dynamic>{
+      'status': _$ReleaseStatusEnumMap[instance.status],
+      'model': instance.model,
+      'type': instance.type,
+      'version': instance.version,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$ReleaseStatusEnumMap = {
+  ReleaseStatus.debug: 'debug',
+  ReleaseStatus.preview: 'preview',
+  ReleaseStatus.released: 'released',
+};
+
+InstanceSpec _$InstanceSpecFromJson(Map<String, dynamic> json) {
+  return InstanceSpec(
+    type: SpecType.fromJson(json['type'] as String),
+    description: json['description'] as String,
+    services: (json['services'] as List<dynamic>)
+        .map((e) => InstanceServiceSpec.fromJson(e as Map<String, dynamic>))
+        .toList(),
+  );
+}
+
+Map<String, dynamic> _$InstanceSpecToJson(InstanceSpec instance) =>
+    <String, dynamic>{
+      'type': instance.type,
+      'description': instance.description,
+      'services': instance.services,
+    };
+
+InstanceServiceSpec _$InstanceServiceSpecFromJson(Map<String, dynamic> json) {
+  return InstanceServiceSpec(
+    iid: json['iid'] as int,
+    type: SpecType.fromJson(json['type'] as String),
+    description: json['description'] as String,
+    properties: (json['properties'] as List<dynamic>?)
+            ?.map(
+                (e) => InstancePropertySpec.fromJson(e as Map<String, dynamic>))
+            .toList() ??
+        [],
+  );
+}
+
+Map<String, dynamic> _$InstanceServiceSpecToJson(
+        InstanceServiceSpec instance) =>
+    <String, dynamic>{
+      'iid': instance.iid,
+      'type': instance.type,
+      'description': instance.description,
+      'properties': instance.properties,
+    };
+
+InstancePropertySpec _$InstancePropertySpecFromJson(Map<String, dynamic> json) {
+  return InstancePropertySpec(
+    iid: json['iid'] as int,
+    type: SpecType.fromJson(json['type'] as String),
+    description: json['description'] as String,
+    format: json['format'] as String,
+  );
+}
+
+Map<String, dynamic> _$InstancePropertySpecToJson(
+        InstancePropertySpec instance) =>
+    <String, dynamic>{
+      'iid': instance.iid,
+      'type': instance.type,
+      'description': instance.description,
+      'format': instance.format,
+    };
